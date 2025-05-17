0812a9a4ee7770285b4730a8db38c9817feca748
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2025-05-17 21:31:20 +0300

    updates in project

--- scripts/index.js
+++ scripts/index.js
@@ -41,3 +41,35 @@ function renderTemplate() {
   });
 }
 renderTemplate();
+
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".popup__close");
+const popupEdit = document.querySelector(".popup_type_edit");
+const addButton = document.querySelector(".profile__add-button");
+const popupNewCard = document.querySelector(".popup_type_new-card");
+const popupImage = document.querySelector(".popup_type_image");
+const popupRemove = document.querySelector(".popup");
+const image = document.querySelector(".card__image");
+
+editButton.addEventListener("click", () => {openPopup(popupEdit)});
+addButton.addEventListener("click", () => {openPopup(popupNewCard)});
+image.addEventListener("click", () => {console.log("OK")});
+
+closeButton.addEventListener("click", () => {closePopup(popupRemove)});
+
+function openPopup(popupWindow) {
+  popupWindow.classList.add("popup_is-opened");
+}
+
+function closePopup(popupWindow) {
+  popupWindow.classList.remove("popup_is-opened");
+}
+
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const formEditProfile = document.forms["edit-profile"];
+const inputName = formEditProfile.elements.name;
+const inputDescription = formEditProfile.elements.description;
+
+inputName.value = profileTitle.textContent;
+inputDescription.value = profileDescription.textContent;
