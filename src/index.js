61ecf1ed200000fa7da5e4c7e504035cc2063d01
Nikolay Korobeynikov <nickkorobeynikov@gmil.com>
2025-05-19 22:16:21 +0300

    gitignore file has been added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const placeList = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function createCard(title, imageUrl, deleteCard) {
+function createCard(title, imageUrl, deleteCard, isLiked) {
   const cardCopy = cardTemplate.cloneNode(true);
   const card = cardCopy.querySelector(".card");
   const cardImage = cardCopy.querySelector(".card__image");
@@ -53,41 +53,79 @@ const popupNewCard = document.querySelector(".popup_type_new-card");
 const popupTypeImage = document.querySelector(".popup_type_image");
 const popupRemove = document.querySelectorAll(".popup");
 const image = document.querySelectorAll(".card__image");
-const popupImage = document.querySelector('.popup__image');
+const popupImage = document.querySelector(".popup__image");
+// const popupTitle = document.querySelector('.popup__caption');
 
-editButton.addEventListener("click", () => {openPopup(popupEdit)});
-addButton.addEventListener("click", () => {openPopup(popupNewCard)});
+editButton.addEventListener("click", () => {
+  openPopup(popupEdit);
+});
+addButton.addEventListener("click", () => {
+  openPopup(popupNewCard);
+});
 
-image.forEach(image => {
-  image.addEventListener('click', function() {
+image.forEach((image) => {
+  image.addEventListener("click", function () {
     const imageSrc = image.src;
     popupImage.src = imageSrc;
-    openPopup(popupTypeImage)
-  })
-})
+    openPopup(popupTypeImage);
+  });
+});
 
-closeButton.forEach(button => {
-  button.addEventListener('click', function() {
-    popupRemove.forEach(popup => {
+
+
+closeButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    popupRemove.forEach((popup) => {
       closePopup(popup);
-    })
-  })
-})
-
+    });
+  });
+});
 
 function openPopup(popupWindow) {
   popupWindow.classList.add("popup_is-opened");
+  document.addEventListener("keydown", closePopupEsc);
 }
 
 function closePopup(popupWindow) {
   popupWindow.classList.remove("popup_is-opened");
 }
 
+function closePopupEsc(evt) {
+  if (evt.key === "Escape") {
+    const popup = document.querySelector(".popup_is-opened");
+    closePopup(popup);
+  }
+}
+
 const profileTitle = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const formEditProfile = document.forms["edit-profile"];
-const inputName = formEditProfile.elements.name;
-const inputDescription = formEditProfile.elements.description;
 
-inputName.value = profileTitle.textContent;
-inputDescription.value = profileDescription.textContent;
+const formElement = document.querySelector('.popup__form');
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_description');
+
+nameInput.value = profileTitle.textContent;
+jobInput.value = profileDescription.textContent;
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  const newName = nameInput.value;
+  const newJob = jobInput.value;
+  profileTitle.textContent = newName;
+  profileDescription.textContent = newJob;
+  closePopup(popupEdit);
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
+
+
+const likeButton = document.querySelectorAll(".card__like-button");
+
+function isLiked() {
+  likeButton.forEach((element) => {
+    element.addEventListener("click", () => {
+      element.classList.toggle("card__like-button_is-active");
+    });
+  });
+}
+isLiked();
